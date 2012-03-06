12a44c3a15282b1df2133b28043b5daee9fe876e
Dmitrijs Milajevs <dimazest@gmail.com>
2012-03-06 15:59:29 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="pystreamgraph",
+      version="0.0.1",
+      description="Stream graph generator",
+      author="Nathan Bergey",
+      author_email="nathan.bergey@gmail.com",
+      url="https://github.com/natronics/pystreamgraph",
+      py_modules=["pystreamgraph"],
+      )
+
